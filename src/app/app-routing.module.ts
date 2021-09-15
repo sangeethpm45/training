1d7eda4be2b708fd05b0cbdba5148b649837686e
sangeethpm45 <sangeethpm45@gmail.com>
2021-09-15 22:08:04 +0530

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculationComponent } from './calculation/calculation.component';
+import { FarmDetailsComponent } from './farm-details/farm-details.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 
 const routes: Routes = [
   {
     path:'',
     component:FrontPageComponent
-  }
+  },
+  {
+    path:'farm-details',
+    component:FarmDetailsComponent
+  },
+  {
+    path:'calculation',
+    component:CalculationComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { HeaderComponent } from './header/header.component';
+import { FarmDetailsComponent } from './farm-details/farm-details.component';
+import { StepMarkComponent } from './step-mark/step-mark.component';
+import { CalculationComponent } from './calculation/calculation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    FarmDetailsComponent,
+    StepMarkComponent,
+    CalculationComponent
   ],
   imports: [
     BrowserModule,
