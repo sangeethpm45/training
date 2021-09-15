8fa5a0f94252881dc38cfcc921b0e096c6ba6465
sangeethpm45 <sangeethpm45@gmail.com>
2021-09-15 15:00:24 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:FrontPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
