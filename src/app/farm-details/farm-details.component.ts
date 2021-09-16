725359c429735bf9440a541540997269a299ba91
sangeethpm45 <sangeethpm45@gmail.com>
2021-09-16 13:03:06 +0530

    kk

--- src/app/farm-details/farm-details.component.ts
+++ src/app/farm-details/farm-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-farm-details',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FarmDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+calculate(){
+this.router.navigateByUrl('calculation')
+}
 }
